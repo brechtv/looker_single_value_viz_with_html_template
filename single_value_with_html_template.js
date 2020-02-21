cc8f7e60973a50d055e49a945136376bddc6334f
Brecht <brechtvermeire@gmail.com>
2020-02-21 15:35:19 +0000

    also for measures

--- single_value_with_html_template.js
+++ single_value_with_html_template.js
@@ -3,7 +3,7 @@ looker.plugins.visualizations.add({
     html_template: {
       type: "string",
       label: "HTML Template",
-      default: `<div style="text-align: center; color: #282828; font-family: 'Open Sans'; font-size: 5rem; font-weight: 700;">{{ value }}</div>`
+      default: `<div style="text-align: center; color: #5A2FC2; font-family: 'Open Sans'; font-size: 5rem; font-weight: 700;">{{ value }}</div>`
     }
   },
  
@@ -13,9 +13,10 @@ looker.plugins.visualizations.add({
     this.clearErrors();
     
     const firstRow = data[0];
-    const firstDimension = firstRow[queryResponse.fields.dimensions[0].name];
-    const firstMeasure = firstRow[queryResponse.fields.measure_like[0].name];
-    const firstCell = firstDimension ? firstDimension : firstMeasure;
+    const qFields = queryResponse.fields;
+
+    const firstCell = firstRow[qFields.dimension_like.length > 0 ? qFields.dimension_like[0].name : qFields.measure_like[0].name];
+
     const htmlForCell = LookerCharts.Utils.htmlForCell(firstCell);
     const htmlTemplate = config && config.html_template || this.options.html_template.default;
     const htmlFormatted = htmlTemplate.replace(/{{.*}}/g, htmlForCell);
