5855deee8be724f3756a0a95fdc94db78facf7d0
Brecht <brechtvermeire@gmail.com>
2020-02-21 15:53:22 +0000

    errors

--- single_value_with_html_template.js
+++ single_value_with_html_template.js
@@ -1,28 +1,44 @@
 looker.plugins.visualizations.add({
-  options: {
-    html_template: {
-      type: "string",
-      label: "HTML Template",
-      default: `<div style="text-align: center; color: #5A2FC2; font-family: 'Open Sans'; font-size: 5rem; font-weight: 700;">{{ value }}</div>`
+    options: {
+        html_template: {
+            type: "string",
+            label: "HTML Template",
+            default: `<div style="text-align: center; color: #5A2FC2; font-family: 'Open Sans'; font-size: 5rem; font-weight: 700;">{{ value }}</div>`
+        }
+    },
+
+    create: function(element, config) {},
+
+    updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+        this.clearErrors();
+
+        const firstRow = data[0];
+        const qFields = queryResponse.fields;
+
+        if (qFields.dimension_like.length === 0 &&
+            qFields.measure_like.length === 0) {
+            this.addError({
+                title: `No visible fields`,
+                message: `At least one dimension, measure or table calculation needs to be visible.`
+            })
+        }
+
+        const firstCell = firstRow[qFields.dimension_like.length > 0 ? qFields.dimension_like[0].name : qFields.measure_like[0].name];
+
+        const htmlForCell = LookerCharts.Utils.filterableValueForCell(firstCell);
+        const htmlTemplate = config && config.html_template || this.options.html_template.default;
+        
+        if (!htmlTemplate || htmlTemplate === "") {
+            this.addError({
+                title: `HTML template is missing`,
+                message: `Add some HTML into the visualisation settings.`
+            })
+        }
+
+        const htmlFormatted = htmlTemplate.replace(/{{.*}}/g, htmlForCell);
+
+        element.innerHTML = htmlFormatted;
+
+        doneRendering();
     }
-  },
- 
-  create: function(element, config) {},
-
-  updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-    this.clearErrors();
-    
-    const firstRow = data[0];
-    const qFields = queryResponse.fields;
-
-    const firstCell = firstRow[qFields.dimension_like.length > 0 ? qFields.dimension_like[0].name : qFields.measure_like[0].name];
-
-    const htmlForCell = LookerCharts.Utils.htmlForCell(firstCell);
-    const htmlTemplate = config && config.html_template || this.options.html_template.default;
-    const htmlFormatted = htmlTemplate.replace(/{{.*}}/g, htmlForCell);
-
-    element.innerHTML = htmlFormatted;
-    
-    doneRendering();
-  }
 });
