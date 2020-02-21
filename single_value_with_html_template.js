8b593ccc3f8bcfb57c906a548556eb8d76516a76
Brecht <brechtvermeire@gmail.com>
2020-02-21 13:51:35 +0000

    cleanup

--- single_value_with_html_template.js
+++ single_value_with_html_template.js
@@ -3,26 +3,22 @@ looker.plugins.visualizations.add({
     html_template: {
       type: "string",
       label: "HTML Template",
-      default: "<div>{{ value }}</div>",
+      default: `<div>{{ value }}</div>`,
       display: "input"
     }
   },
  
-  create: function(element, config){
-    element.innerHTML = "<h1>Ready to render!</h1>";
-  },
+  create: function(element, config) {},
 
   updateAsync: function(data, element, config, queryResponse, details, doneRendering){
     this.clearErrors();
-    
-    console.log(config.html_template)
     
     const firstRow = data[0];
     const firstCell = firstRow[queryResponse.fields.dimensions[0].name];
     const htmlForCell = LookerCharts.Utils.htmlForCell(firstCell);
     const htmlTemplate = config && config.html_template || this.options.html_template.default;
 
-    const htmlFormatted = html_template.replace(/{{.*}}/g, htmlForCell);
+    const htmlFormatted = htmlTemplate.replace(/{{.*}}/g, htmlForCell);
     element.innerHTML = htmlFormatted;
     
     doneRendering();
